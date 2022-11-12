241a46da6dfc4f240c05048c41444169a163bf55
Takeshi <sr-tmiura@mx2.toyotasystems.com>
2022-11-12 17:24:38 +0900

    アイコンとか

--- GridSample/Controllers/HomeController.cs
+++ GridSample/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace GridSample.Controllers
             return View();
         }
 
+        public IActionResult Grid()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
