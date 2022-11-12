9888bfe00fcdd7131996829b622362a7ae9dea92
Takeshi <sr-tmiura@mx2.toyotasystems.com>
2022-11-12 15:31:33 +0900

    ひな形

---
+++ GridSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GridSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
